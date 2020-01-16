a8a883e6b59dca7d5b76fb948feccf4dcbea9eed
Bruno Serrano <bruno.santos@supera.com.br>
2020-01-15 23:08:38 -0300

    Project created

---
+++ GitHub-CI/GitHub-CI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHub-CI
+//
+//  Created by Bruno Serrano dos Santos on 15/01/20.
+//  Copyright © 2020 EagleSoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GitHub-CI/GitHub-CITests/GitHub_CITests.swift
@@ -0,0 +1,34 @@
+//
+//  GitHub_CITests.swift
+//  GitHub-CITests
+//
+//  Created by Bruno Serrano dos Santos on 15/01/20.
+//  Copyright © 2020 EagleSoft. All rights reserved.
+//
+
+import XCTest
+@testable import GitHub_CI
+
+class GitHub_CITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
